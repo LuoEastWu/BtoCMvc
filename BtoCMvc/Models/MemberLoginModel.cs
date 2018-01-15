9238856cff4054e03c57e8c494ea95b2c428e7e2
Luo <Luo@DESKTOP-42JM05A>
2018-01-15 18:15:39 +0800

    555

---
+++ BtoCMvc/Models/MemberLoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+
+namespace BtoCMvc.Models
+{
+    public class MemberLoginModel
+    {
+        [Display(Name = "邮箱/手机/用户名")]
+        public String User { get; set; }
+        [Display(Name = "登录密码")]
+        public String Password { get; set; }
+    }
+}
